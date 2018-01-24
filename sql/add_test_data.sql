3a95077c227365fd0495b4ba6bec8906e68198d5
tattimus <samu.tatti@luukku.com>
2018-01-24 16:14:20 +0200

    Taulujen luonti ja testidatan lisäys

--- sql/add_test_data.sql
+++ sql/add_test_data.sql
@@ -1,1 +1,4 @@
--- Lisää INSERT INTO lauseet tähän tiedostoon
+INSERT INTO Kayttaja (tunnus, salasana) VALUES ('Samu', 'Samu123');
+INSERT INTO Trilogia (nimi, arvio, media, sanaArvio) VALUES ('Matrix-trilogia', '4,7', 'Elokuva', 'Loistava toiminta trilogia mielenkiintoisella tarinalla.');
+INSERT INTO TrilogianOsa (nimi, arvio, media, julkaistu, sanaArvio) VALUES ('The Matrix', '4,9', 'Elokuva', '31.3.1999', 'Trilogian ensimmäinen osa on ehdottomasti paras. Hyvin tahditettu ja mukaansa tempaava.');
+INSERT INTO Genre (nimi) VALUES ('Sci-fi', 'Toiminta');
--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -1,1 +1,41 @@
 -- Lisää CREATE TABLE lauseet tähän tiedostoon
+CREATE TABLE Kayttaja(
+  id SERIAL PRIMARY KEY,
+  tunnus varchar(20) NOT NULL,
+  salasana varchar(20) NOT NULL,
+);
+
+CREATE TABLE Trilogia(
+  id SERIAL PRIMARY KEY,
+  kayttaja_id INTEGER REFERENCES Kayttaja(id),
+  nimi varchar(50) NOT NULL,
+  arvio DECIMAL(1,1) NOT NULL,
+  media varchar(15) NOT NULL,
+  sanaArvio varchar(300)
+);
+
+CREATE TABLE TrilogianOsa(
+  id SERIAL PRIMARY KEY,
+  trilogia_id INTEGER REFERENCES Trilogia(id),
+  kayttaja_id INTEGER REFERENCES Kayttaja(id),
+  nimi varchar(50) NOT NULL,
+  arvio DECIMAL(1,1) NOT NULL,
+  media varchar(15) NOT NULL,
+  julkaistu DATE NOT NULL,
+  sanaArvio varchar(300)
+);
+
+CREATE TABLE Genre(
+  id SERIAL PRIMARY KEY,
+  nimi varchar(15)
+);
+
+CREATE TABLE GenreLiitosT(
+  trilogia_id INTEGER REFERENCES Trilogia(id),
+  genre_id INTEGER REFERENCES Genre(id)
+);
+
+CREATE TABLE GenreLiitosO(
+  osa_id INTEGER REFERENCES TrilogianOsa(id),
+  genre_id INTEGER REFERENCES Genre(id)
+);
