43ba7ad357c68e5d1b4ca5241f964b7f0bed0754
tattimus <samu.tatti@luukku.com>
2018-01-24 16:41:16 +0200

    update

--- sql/add_test_data.sql
+++ sql/add_test_data.sql
@@ -1,4 +1,5 @@
 INSERT INTO Kayttaja (tunnus, salasana) VALUES ('Samu', 'Samu123');
 INSERT INTO Trilogia (nimi, arvio, media, sanaArvio) VALUES ('Matrix-trilogia', '4,7', 'Elokuva', 'Loistava toiminta trilogia mielenkiintoisella tarinalla.');
-INSERT INTO TrilogianOsa (nimi, arvio, media, julkaistu, sanaArvio) VALUES ('The Matrix', '4,9', 'Elokuva', '31.3.1999', 'Trilogian ensimmäinen osa on ehdottomasti paras. Hyvin tahditettu ja mukaansa tempaava.');
-INSERT INTO Genre (nimi) VALUES ('Sci-fi', 'Toiminta');
+INSERT INTO TrilogianOsa (nimi, moneskoOsa, arvio, media, julkaistu, sanaArvio) VALUES ('The Matrix', '1', '4,9', 'Elokuva', '31.3.1999', 'Trilogian ensimmäinen osa on ehdottomasti paras. Hyvin tahditettu ja mukaansa tempaava.');
+INSERT INTO Genre (nimi) VALUES ('Toiminta');
+INSERT INTO Genre (nimi) VALUES ('Sci-fi');
